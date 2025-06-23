d3a6a412eb9a4f8c78a444cb60384f3510784b7b
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-24 00:37:42 +0300

    feat: added sqlconfig file

---
+++ vendor-service/internal/shared/db/sqlconfig.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"marketplace-vendor-service/vendor-service/internal/shared/utils/error_handler"
+	"os"
+)
+
+func ConnectDb() (*gorm.DB, error) {
+
+	if err := godotenv.Load(); err != nil {
+		return nil, error_handler.ErrorHandler(err, err.Error())
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	host := os.Getenv("DB_HOST")
+
+	dataSourceName := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require", host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+
+	if err != nil {
+		return nil, error_handler.ErrorHandler(err, err.Error())
+	}
+
+	return db, nil
+
+}
