62a676a0ee2678d5beb23aea4c1627c9191d71a7
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-09 00:43:18 +0300

    feat: updated handler struct

--- vendor-service/internal/shared/handler/handler_struct.go
+++ vendor-service/internal/shared/handler/handler_struct.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"gorm.io/gorm"
 	accountDomain "marketplace-vendor-service/vendor-service/internal/account/domain"
+	eventDomain "marketplace-vendor-service/vendor-service/internal/event/domain"
 	orderDomain "marketplace-vendor-service/vendor-service/internal/orders/domain"
 	reviewDomain "marketplace-vendor-service/vendor-service/internal/reviews/domain"
 )
@@ -13,6 +14,8 @@ type Handler struct {
 	ReviewRepo reviewDomain.ReviewRepository
 
 	OrderRepo orderDomain.OrderRepository
+
+	EventRepo eventDomain.EventRepository
 
 	Db *gorm.DB
 }
