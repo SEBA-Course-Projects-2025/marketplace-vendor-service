a3d6cc613f749c648a320794e35492568fc72d50
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-09 00:44:44 +0300

    feat: added tracer implementation for the vendor service

---
+++ vendor-service/internal/shared/tracer/tracer.go
@@ -0,0 +1,54 @@
+package tracer
+
+import (
+	"context"
+	"encoding/base64"
+	"github.com/joho/godotenv"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"log"
+	"os"
+)
+
+var Tracer = otel.Tracer("vendor-service")
+
+func InitTracer() func(ctx context.Context) error {
+
+	ctx := context.Background()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	stackId := os.Getenv("GRAFANA_TEMPO_STACK_ID")
+	apiKey := os.Getenv("GRAFANA_TEMPO_API_KEY")
+	endpoint := os.Getenv("GRAFANA_TEMPO_ENDPOINT")
+
+	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(stackId+":"+apiKey))
+
+	exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpoint(endpoint), otlptracehttp.WithHeaders(map[string]string{
+		"Authorization": auth,
+	}), otlptracehttp.WithURLPath("otlp/v1/traces"))
+
+	if err != nil {
+		log.Fatalf("Error creating OpenTelemetry exporter: %v", err)
+	}
+
+	otelRes, err := resource.New(ctx, resource.WithAttributes(semconv.ServiceName("vendor-product-service")))
+
+	if err != nil {
+		log.Fatalf("Error creating OpenTelemetry resource: %v", err)
+	}
+
+	provider := trace.NewTracerProvider(trace.WithSyncer(exporter), trace.WithResource(otelRes))
+
+	otel.SetTracerProvider(provider)
+
+	Tracer = otel.Tracer("vendor-service")
+
+	return provider.Shutdown
+
+}
