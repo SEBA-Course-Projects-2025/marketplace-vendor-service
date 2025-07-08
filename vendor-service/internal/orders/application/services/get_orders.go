8e1eb1ec418f72be595ef2c715b527537c21ff2b
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-09 00:42:53 +0300

    feat: added tracing support for getting orders application service

--- vendor-service/internal/orders/application/services/get_orders.go
+++ vendor-service/internal/orders/application/services/get_orders.go
@@ -5,9 +5,13 @@ import (
 	"github.com/google/uuid"
 	"marketplace-vendor-service/vendor-service/internal/orders/domain"
 	"marketplace-vendor-service/vendor-service/internal/orders/dtos"
+	"marketplace-vendor-service/vendor-service/internal/shared/tracer"
 )
 
 func GetOrders(ctx context.Context, orderRepo domain.OrderRepository, params dtos.OrderQueryParams, vendorId uuid.UUID) ([]dtos.GetOrdersResponse, error) {
+
+	ctx, span := tracer.Tracer.Start(ctx, "GetOrders")
+	defer span.End()
 
 	orders, err := orderRepo.FindAll(ctx, params, vendorId)
 
