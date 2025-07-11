952a120cb21415cfd27be81cc8ddccb32fd55ae9
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-07-11 11:08:26 +0300

    feat: updated general router to support metrics

--- vendor-service/internal/shared/router/general_router.go
+++ vendor-service/internal/shared/router/general_router.go
@@ -3,8 +3,11 @@ package router
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	_ "marketplace-vendor-service/docs"
 	accountInterfaces "marketplace-vendor-service/vendor-service/internal/account/interfaces"
 	accountHandlers "marketplace-vendor-service/vendor-service/internal/account/interfaces/handlers"
@@ -13,11 +16,22 @@ import (
 	reviewInterfaces "marketplace-vendor-service/vendor-service/internal/reviews/interfaces"
 	reviewHandlers "marketplace-vendor-service/vendor-service/internal/reviews/interfaces/handlers"
 	"marketplace-vendor-service/vendor-service/internal/shared/middlewares"
+	"os"
 )
 
 func SetUpRouter(accountHandler *accountHandlers.AccountHandler, reviewHandler *reviewHandlers.ReviewHandler, orderHandler *orderHandlers.OrderHandler) *gin.Engine {
 
 	r := gin.New()
+
+	p := ginprometheus.NewPrometheus("vendor_service")
+	p.MetricsPath = ""
+	p.Use(r)
+
+	r.GET("/metrics", gin.BasicAuth(gin.Accounts{
+		os.Getenv("METRICS_ACCESS_USERNAME"): os.Getenv("METRICS_ACCESS_PASSWORD"),
+	}), gin.WrapH(promhttp.Handler()))
+
+	r.Use(otelgin.Middleware("vendor_service"))
 
 	r.Use(cors.New(cors.Config{
 		AllowAllOrigins: true,
